f0800d9dea7aad5d6cecdf4cc96cd9cff5bb8186
JohanSannier <johan.sanniercaffier@gmail.com>
2022-07-22 12:05:26 +0200

    Nav et footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Footer(props) {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      {" "}
+      <p class="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
+  );
 }
 
 export default Footer;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import argentBankLogo from "../img/argentBankLogo.png";
 
 function Navbar(props) {
   return (
-    <nav>
-      <Link to="/">
-        <img src="#" alt="logo" />
+    <nav class="main-nav">
+      <Link to="/" className="main-nav-logo">
+        <img
+          class="main-nav-logo-image"
+          src={argentBankLogo}
+          alt="Argent Bank Logo"
+        />
+        <h1 class="sr-only">Argent Bank</h1>
       </Link>
-
       <div>
-        <Link to="/login">Sign In</Link>
+        <Link to="/login" className="main-nav-item">
+          <FontAwesomeIcon icon={faUserCircle} />
+          <span className="sign-in-span">Sign In</span>
+        </Link>
       </div>
     </nav>
   );
